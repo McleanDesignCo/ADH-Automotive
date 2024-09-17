e21d30d49869cb695713adb6586dfe50278c5d38
Made by Mclean <joshua@madebymclean.com>
2024-09-17 17:17:08 +0100

    Favicon and SEO upgrade

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "09d6434098dcd8c2"; }
+/******/ 		__webpack_require__.h = function() { return "e8deffdcc665df00"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: {
     template: '%s - ADH Automotive',
-    default: 'ADH Automotive - Car Repair in Digbeth, Birmingham.',
+    default: 'ADH Automotive - Expert Vehicle Service & Repair in Digbeth, Birmingham City Centre',
   },
   description:
-    'Insert website description here',
+    'ADH Automotive offers top-notch vehicle service and repair in Digbeth and Birmingham City Centre. With over 10 years of experience, we provide professional and competitive auto services to keep your vehicle in prime condition. Discover our comprehensive range of services and get in touch today!',
 }
 
 export default function RootLayout({
