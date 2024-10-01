d189639ea6d9bb104928c6c80ed9f51c408cd9d1
Made by Mclean <joshua@madebymclean.com>
2024-10-01 16:52:14 +0100

    Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import clsx from 'clsx'
+import { Analytics } from "@vercel/analytics/react"
 
 import '@/styles/tailwind.css'
 
@@ -26,7 +27,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={clsx('bg-gray-50 antialiased scroll-smooth', inter.variable)}>
-      <body>{children}</body>
+      
+      <body>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
